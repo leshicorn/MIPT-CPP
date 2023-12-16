6009f2569e03fea4e3c62a17bf0221c1b37a4293
leshicorn <romanenkov2003@mail.ru>
2023-12-16 17:46:19 +0300

    final test fixed

--- final_test/1.cpp
+++ final_test/1.cpp
@@ -2,32 +2,40 @@
 #include <string>
 
 using namespace std;
-
-void printResult(const string& secretWord, const string& guess) {
-    for (size_t i = 0; i < secretWord.length(); ++i) {
-        if (secretWord[i] == guess[i]) {
-            cout << 'g';
-        } else if (secretWord.find(guess[i]) != string::npos) {
-            cout << 'y';
-        } else {
-            cout << '-';
+class Game {
+public:
+    void printResult(const string& secretWord, const string& guess) {
+        for (size_t i = 0; i < secretWord.length(); ++i) {
+            if (secretWord[i] == guess[i]) {
+                cout << 'g';
+            } else if (secretWord.find(guess[i]) != string::npos) {
+                cout << 'y';
+            } else {
+                cout << '-';
+            }
         }
+        cout << endl;
     }
-    cout << endl;
-}
+
+    void run_game() {
+        string secretWord;
+        int attempts;
+
+        cin >> secretWord >> attempts;
+
+        for (int i = 0; i < attempts; ++i) {
+            string guess;
+            cin >> guess;
+
+            printResult(secretWord, guess);
+        }
+        return;
+    }
+};
 
 int main() {
-    string secretWord;
-    int attempts;
 
-    cin >> secretWord >> attempts;
-
-    for (int i = 0; i < attempts; ++i) {
-        string guess;
-        cin >> guess;
-
-        printResult(secretWord, guess);
-    }
-
+    Game game;
+    game.run_game();    
     return 0;
 }
